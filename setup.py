f00c63a9ff5b4164c5b7d911916c3039855ba399
saikaushik1997 <saikaushik.saikz@gmail.com>
2021-08-30 20:05:56 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,6 @@
 # Setup file
-python setup.py install
+from setuptools import setup
+
+setup( name='Team20_SE_Project', version='1.0', description='A useful module', author='Man Foo', author_email='foomail@foo.com', packages=['Team20_SE_Project'], #same as name 
+      install_requires=['wheel', 'bar', 'greek'], #external packages as dependencies
+     )
