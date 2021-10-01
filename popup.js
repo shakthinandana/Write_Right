afbb7259d21aa6d8b01af306c78ad9aea1332e49
Kailash Singaravelu <kailash98s@gmail.com>
2021-09-30 23:22:49 -0400

    Modified code to accomodate edge cases

--- popup.js
+++ popup.js
@@ -1,7 +1,4 @@
-/*.............................................
-Functionality: Declarations
-.............................................*/
-let findBtn = $('#submit');                   
+let findBtn = $('#submit');
 let meaning = $('#meaning');
 let synonyms = $('#synonyms');
 let antonyms = $('#antonyms');
@@ -13,15 +10,7 @@ let Destination = $('.Destination');
 let submit = $('#submit');
 base_url = ''
 
-/*.............................................
-Input: Takes the Source and Destination for checking.
-
-Functionality: Finds ISO standards for given language.
-
-Output: Gives the ISO Standard name for the given 
-inputs.
-.............................................*/
-language_dict = {
+const language_dict = {
    "achinese": "ace",
    "adyghe": "ady",
    "afrikaans": "af",
@@ -396,295 +385,230 @@ language_dict = {
    "yoruba": "yo",
    "zaza": "zza",
    "zulu": "zu"
-}
+};
 
-// chrome.tabs.executeScript(integer tabId(optional, default: current window), object details, function callback)
-
-/*.............................................
-Input: Highlighted text.
-
-Functionality: Selects the highlighted word 
-from web page and displays it in search box.
-
-Output: Triggers wordToSearch.
-.............................................*/
 chrome.tabs.executeScript({
-    code: 'window.getSelection().toString();'
+   code: 'window.getSelection().toString();'
 },
-    selection => {
-        console.log(selection[0]);
-        if (selection)
-            wordToSearch.val(selection[0]);
-        else
-            wordToSearch.val("");
-    }
-);
-
-/*.............................................
-Input: Shortcut hit.
-
-Functionality: Triggered when Alt+S or Option+S
-is clicked on a web page.
-
-Output: Triggers findBtn.
-.............................................*/
-document.addEventListener('keydown', key => {
-    let code = key.keyCode;
-    if (code == 13) {
-        console.log("Enter key pressed");
-        meaning.empty();
-        synonyms.empty();
-        antonyms.empty();
-        base_url = ''
-        if (wordToSearch.val() === "") {
-            meaning.css('display', 'none');
-            synonyms.css('display', 'none');
-            antonyms.css('display', 'none');
-            audio1.css('display', 'none')
-            error.css('display', 'block');
-            error.text('Please enter a word');
-
-        } else {
-            findBtn.click();
-        }
-    }
+selection => {
+   console.log(selection[0]);
+   if (selection)
+      wordToSearch.val(selection[0]);
+   else
+      wordToSearch.val("");
 });
 
-/*.............................................
-Input: Value from wordToSearch.
+document.addEventListener('keydown', key => {
+   let code = key.keyCode;
+   if (code == 13) {
+      console.log("Enter key pressed");
+      meaning.empty();
+      synonyms.empty();
+      antonyms.empty();
+      base_url = ''
+      if (wordToSearch.val() === "") {
+         meaning.css('display', 'none');
+         synonyms.css('display', 'none');
+         antonyms.css('display', 'none');
+         audio1.css('display', 'none');
+         error.css('display', 'block');
+         error.text('Please enter a word');
+      } else {
+         findBtn.click();
+      }
+   }
+});
 
-Functionality: Confirms that an input is
-given and triggers the main functionality.
-
-Output:Triggers handleSubmit.
-.............................................*/
 findBtn.click(() => {
-    meaning.empty();
-    synonyms.empty();
-    antonyms.empty();
-    base_url = '';
-    error.css('display', 'none')
-    meaning.css('display', 'none')
-    synonyms.css('display', 'none');
-    antonyms.css('display', 'none');
-    audio1.css('display', 'none');
-    if (wordToSearch.val() === "") {
-        meaning.empty();
-        error.css('display', 'block');
-        error.text('Please enter a word');
+   meaning.empty();
+   synonyms.empty();
+   antonyms.empty();
+   base_url = '';
+   error.css('display', 'none')
+   meaning.css('display', 'none')
+   synonyms.css('display', 'none');
+   antonyms.css('display', 'none');
+   audio1.css('display', 'none');
+   if (wordToSearch.val() === "") {
+      meaning.empty();
+      error.css('display', 'block');
+      error.text('Please enter a word');
+   } else {
+      handleSubmit();
+   }
+});
 
-    } else {
-        handleSubmit();
-    }
-})
-
-/*.............................................
-Input: Word by user
-
-Description: Main Functionality.
-Handles errors such as no reply from dictionary etc.
-Calls API and retrieves required information.
-
-Output: Short Definition, Synonyms, Antonyms sent 
-to user.
-If translation is requested, sends the word 
-translated to the user.
-.............................................*/
 function handleSubmit() {
-    let str = String(wordToSearch.val()).toLowerCase();
-    if(document.getElementById("submit").innerText === "Search"){
-        console.log(str);
-        const url = 'https://www.dictionaryapi.com/api/v3/references/thesaurus/json/' + str + '?key=05263739-8ae9-45fe-842c-3a37ed0f181b';
-        fetch(url)
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                if (Object.keys(data).length === 0) {
-                    error.css('display', 'block');
-                    error.text('No such word was found');
-
-                } else if (data[0]['meta'] === undefined) {
-                    error.css('display', 'block');
-                    error.text('Word not found. Did you mean to type any of these?');
-
-                    synonyms.css('display', 'block');
-                    for (var i = 0; i < data.length; i++) {
-                        let mainNear = `<li> <ol id="subNear${i}"> </ol> </li>`;
-                        synonyms.append(mainNear);
-                    }
-
-                    for (var k = 0; k < data.length; k++) {
-                        var nearWords = data[k];
-
-                        let txtNear = `<li> ${nearWords} </li>`;
-                        $('#subNear' + k).append(txtNear)
-                    }
-
-
-                } else {
-                    meaning.css('display', 'block');
-                    meaning.append('Searching ...');
-                    meaning.empty();
-                    checkAudio();
-                    for (var i = 0; i < data.length; i++) {
-                        if(data[i].meta.id == str){
-                            let txtMain = `<li> <strong style="text-transform: capitalize">${data[i].meta.id}</strong>
-                            <i> ${data[i].fl}</i><li id="sublist${i}"> </li> </li>`;
-                            meaning.append(txtMain);
-                        }
-                    }
-
-                    synonyms.css('display', 'block');
-                    synonyms.append('<li><strong style="text-transform: capitalize">Synonyms</strong></li>');
-                    for (var i = 0; i < data.length; i++) {
-      
-                      if(data[i].meta.id == str){
-                          let mainSyn = `<li> <ul id="subSyn${i}"> </ul> </li>`;
-                          synonyms.append(mainSyn);
-                      }
-                    }
-
-                    antonyms.css('display', 'block');
-                    antonyms.append('<li><strong style="text-transform: capitalize">Antonyms</strong></li>');
-                    for (var i = 0; i < data.length; i++) {
-                      
-                      if(data[i].meta.id == str){
+   let str = String(wordToSearch.val()).toLowerCase();
+   if(document.getElementById("submit").innerText === "Search"){
+      console.log(str);
+      const url = 'https://www.dictionaryapi.com/api/v3/references/thesaurus/json/' + str + '?key=05263739-8ae9-45fe-842c-3a37ed0f181b';
+      fetch(url)
+      .then(res => res.json())
+      .then(data => {
+         console.log(data);
+         if (Object.keys(data).length === 0) {
+            error.css('display', 'block');
+            error.text('No such word was found');
+         } else if (data[0]['meta'] === undefined) {
+            error.css('display', 'block');
+            error.text('Word not found. Did you mean to type any of these?');
+            synonyms.css('display', 'block');
+            for (var i = 0; i < data.length; i++) {
+               let mainNear = `<li> <ol id="subNear${i}"> </ol> </li>`;
+               synonyms.append(mainNear);
+            }
+            for (var k = 0; k < data.length; k++) {
+               var nearWords = data[k];
+               let txtNear = `<li> ${nearWords} </li>`;
+               $('#subNear' + k).append(txtNear)
+            }
+         } else {
+            meaning.css('display', 'block');
+            meaning.append('Searching ...');
+            meaning.empty();
+            checkAudio();
+            var meaning_count = 0;
+            for (var i = 0; i < data.length; i++) {
+               if(data[i].meta.id == str){
+                  meaning_count += 1;
+                  let txtMain = `<li> <strong style="text-transform: capitalize">${data[i].meta.id}</strong>
+                  <i> ${data[i].fl}</i><li id="sublist${i}"> </li> </li>`;
+                  meaning.append(txtMain);
+               }
+            }
+            if (meaning_count == 0) {
+               meaning.css('display', 'none');
+               error.css('display', 'block');
+               error.text('Meanings do not exist in database');
+            } else {
+               synonyms.css('display', 'block');
+               synonyms.append(`<li><strong style="text-transform: capitalize">Synonyms</strong></li>`);
+               var synonyms_count = 0;
+               for (var i = 0; i < data.length; i++) {
+                  if(data[i].meta.id == str){
+                     if (data[i].meta.syns.length != 0) {
+                        synonyms_count += 1;
+                        let mainSyn = `<li> <ul id="subSyn${i}"> </ul> </li>`;
+                        synonyms.append(mainSyn);
+                     }
+                  }
+               }
+               if (synonyms_count == 0) {
+                  synonyms.append('Synonyms do not exist in database');
+               }
+               antonyms.css('display', 'block');
+               antonyms.append(`<li><strong style="text-transform: capitalize">Antonyms</strong></li>`);
+               var antonyms_count = 0;
+               for (var i = 0; i < data.length; i++) {
+                  if(data[i].meta.id == str){
+                     if (data[i].meta.ants.length != 0) {
+                        console.log(data[i].meta.ants)
+                        antonyms_count += 1;
                         let mainAnt = `<li> <ul id="subAnt${i}"> </ul> </li>`;
                         antonyms.append(mainAnt);
-                      }
-                    }
-
-                    for (var k = 0; k < data.length; k++) {
-                        var type = data[k]["fl"];
-                        var def = data[k]["shortdef"];
-
-                        let txtSub = `<li> ${def} </li>`;
-                        $('#sublist' + k).append(txtSub)
-                    }
-
-                    for (var k = 0; k < data.length; k++) {
-                        for (j = 0; j < data[k]['meta']['syns'].length; j++) {
-                            var syns = data[k]['meta']['syns'][j];
-
-                            syns = syns.slice(0, 5);
-                            let txtSyn = `<li> ${syns} [<em>${type}</em>] </li>`;
-                            $('#subSyn' + k).append(txtSyn)
-                        }
-
-                    }
-
-                    labelAnts:
-                    for (var k = 0; k < data.length; k++) {
-                        for (j = 0; j < data[k]['meta']['ants'].length; j++) {
-                            var ants = data[k]['meta']['ants'][j];
-
-                            ants = ants.slice(0, 5);
-                            if (ants.length !== 0) {
-                                let txtAnt = `<li> ${ants} [<em>${type}</em>] </li>`;
-                                $('#subAnt' + k).append(txtAnt)
-                            } else {
-                                antonyms.css('display', 'none');
-                                break labelAnts;
-                            }
-                        }
-
-                    }
-
-                }
-            })
-            .catch(err => {
-                console.log('error: ', err);
-            })
-        } else {
-            in_lang = language_dict[String(document.getElementById("Source").value).toLowerCase()]
-            out_lang = language_dict[String(document.getElementById("Destination").value).toLowerCase()]
-            if (in_lang == out_lang) {
-                error.css('display', 'block')
-                error.text('Enter 2 distinct languages')
+                     }
+                  }
+               }
+               console.log(antonyms_count)
+               if (antonyms_count == 0) {
+                  antonyms.append(`<li>Antonyms do not exist in database</li>`);
+               }
+               for (var k = 0; k < data.length; k++) {
+                  var type = data[k]["fl"];
+                  var def = data[k]["shortdef"];
+                  let txtSub = `<li> ${def} </li>`;
+                  $('#sublist' + k).append(txtSub)
+               }
+               for (var k = 0; k < data.length; k++) {
+                  for (j = 0; j < data[k]['meta']['syns'].length; j++) {
+                     var syns = data[k]['meta']['syns'][j];
+                     syns = syns.slice(0, 5);
+                     let txtSyn = `<li> ${syns} [<em>${type}</em>] </li>`;
+                     $('#subSyn' + k).append(txtSyn)
+                  }
+               }
+               labelAnts:
+               for (var k = 0; k < data.length; k++) {
+                  for (j = 0; j < data[k]['meta']['ants'].length; j++) {
+                     var ants = data[k]['meta']['ants'][j];
+                     ants = ants.slice(0, 5);
+                     let txtAnt = `<li> ${ants} [<em>${type}</em>] </li>`;
+                     $('#subAnt' + k).append(txtAnt)
+                  }
+               }
+            }
+         }
+      })
+      .catch(err => {
+         console.log('error: ', err);
+      })
+   } else {
+      in_lang = language_dict[String(document.getElementById("Source").value).toLowerCase()]
+      out_lang = language_dict[String(document.getElementById("Destination").value).toLowerCase()]
+      if (in_lang == out_lang) {
+         error.css('display', 'block')
+         error.text('Enter 2 distinct languages')
+      } else {
+         const url = 'https://api.mymemory.translated.net/get?q=' + str + '!&langpair=' + in_lang + '|' + out_lang
+         fetch(url)
+         .then(res => res.json())
+         .then(data => {
+            console.log(data.responseData);
+            if (data.responseData.hasOwnProperty('match')) {
+               meaning.css('display', 'block')
+               meaning.append(data.responseData.translatedText)
             } else {
-                const url = 'https://api.mymemory.translated.net/get?q=' + str + '!&langpair=' + in_lang + '|' + out_lang
-                fetch(url)
-                    .then(res => res.json())
-                    .then(data => {
-                        console.log(data.responseData);
-                        if (data.responseData.hasOwnProperty('match')) {
-                            meaning.css('display', 'block')
-                            meaning.append(data.responseData.translatedText)
-                        } else {
-                            error.css('display', 'block')
-                            error.text('Input and output languages incorrect')
-                        }
-                    })
-                }
-        }
+               error.css('display', 'block')
+               error.text('Invalid Input/Output language pair')
+            }
+         })
+      }
+   }
 }
 
-/*.............................................
-Input: Audio request.
-
-Functionality: Triggered when the audio button
-is clicked. 
-
-Output: Returns the location of sound output.
-Calls playAudio.
-.............................................*/
 document.getElementById("playaudio").addEventListener("click", playAudio);
 
 function checkAudio() {
-  const url = 'https://dictionaryapi.com/api/v3/references/collegiate/json/' + wordToSearch.val() + '?key=7a38e9d2-6aee-4970-b660-dfa0109b8579'
-  fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            if (data[0].hasOwnProperty('hwi')) {
-                audio1.css('display', 'block');
-                var basefile = data[0].hwi.prs[0].sound.audio;
-                base_url = 'https://media.merriam-webster.com/audio/prons/en/us/mp3/' +basefile[0]+ '/' + basefile + '.mp3';
-            } else {
-                audio1.css('display', 'none');            
-            }
-        })
-  }
+   const url = 'https://dictionaryapi.com/api/v3/references/collegiate/json/' + wordToSearch.val() + '?key=7a38e9d2-6aee-4970-b660-dfa0109b8579'
+   fetch(url)
+   .then(res => res.json())
+   .then(data => {
+      console.log(data);
+      if (data[0].hasOwnProperty('hwi')) {
+         audio1.css('display', 'block');
+         var basefile = data[0].hwi.prs[0].sound.audio;
+         base_url = 'https://media.merriam-webster.com/audio/prons/en/us/mp3/' +basefile[0]+ '/' + basefile + '.mp3';
+      } else {
+         audio1.css('display', 'none');            
+      }
+   })
+}
 
-/*.............................................
-Input: Audio file.
-
-Functionality: Plays the audio file of word.
-
-Output: mp3 file played to user.
-.............................................*/
 function playAudio() {
-  var audio = new Audio(base_url);
-  return audio.paused ? audio.play() : audio.pause();
-  }
+   var audio = new Audio(base_url);
+   return audio.paused ? audio.play() : audio.pause();
+}
 
-/*.............................................
-Input: Checkbox click from user.
-
-Functionality: Triggered when Translate 
-option is checked.
-Allows user to add Source and Destination.
-
-Output: Calls translate or search functionality.
-.............................................*/
 document.getElementById("Translate").addEventListener("click", displaychange);
 
 function displaychange() {
-    if (Translate.checked) {
-        meaning.empty();
-        synonyms.empty();
-        antonyms.empty();
-        base_url = '';
-        error.css('display', 'none')
-        meaning.css('display', 'none')
-        synonyms.css('display', 'none');
-        antonyms.css('display', 'none');
-        audio1.css('display', 'none');
-        document.getElementById("submit").innerText= "Translate";
-        Source.css('display', 'inline-block');
-    } else {
-        Source.css('display', 'none');
-        document.getElementById("submit").innerText= "Search";
-    }
+   if (Translate.checked) {
+      meaning.empty();
+      synonyms.empty();
+      antonyms.empty();
+      base_url = '';
+      error.css('display', 'none')
+      meaning.css('display', 'none')
+      synonyms.css('display', 'none');
+      antonyms.css('display', 'none');
+      audio1.css('display', 'none');
+      document.getElementById("submit").innerText= "Translate";
+      Source.css('display', 'inline-block');
+   } else {
+      meaning.empty();
+      meaning.css('display', 'none')
+      Source.css('display', 'none');
+      document.getElementById("submit").innerText= "Search";
+   }
 }
-
