ef99b90fc957bdc8f4147b54ec717b75e90a6b28
ithaaswin <89421200+ithaaswin@users.noreply.github.com>
2021-09-30 23:07:32 -0400

    Delete setup.py

--- setup.py
+++
@@ -1,6 +0,0 @@
-# Setup file
-from setuptools import setup
-
-setup( name='Team20_SE_Project', version='1.0', description='A useful module', author='Man Foo', author_email='foomail@foo.com', packages=['Team20_SE_Project'], #same as name 
-      install_requires=['wheel', 'bar', 'greek'], #external packages as dependencies
-     )
